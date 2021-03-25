8b7dd3fe5a9a5b414f878e38964c803ba236161c
AJ Vrieland <zer0relm@gmail.com>
2021-03-25 17:55:42 -0400

    vacc_clinic, finished

--- vacc_clinic/vacc_clinic.c
+++ vacc_clinic/vacc_clinic.c
@@ -1,6 +1,8 @@
 /*
  * Name: AJ Vrieland (ajv234)
- * Date: 3-22-21
+ * Date: 3-25-21
+ * 
+ * Use make to compile then ./main to run
  */
 
 #include <pthread.h>
@@ -11,28 +13,35 @@
 #include <time.h>
 #include <unistd.h>
 
-#define DEBUG 1
+//No debug was used in this version, because I restarted on a clean slate to try to get unstuck
 
-// #define NUM_VIALS 30 // Given number
-#define NUM_VIALS 10 //so it doesn't run forever when writing code
+
+
+#define NUM_VIALS 30 // Given number
+//#define NUM_VIALS 10 //Smaller values to shorten testing
 #define SHOTS_PER_VIAL 6
-// #define NUM_CLIENTS (NUM_VIALS * SHOTS_PER_VIAL) 
-#define NUM_CLIENTS 10 //so it doesn't run forever when writing code
+#define NUM_CLIENTS (NUM_VIALS * SHOTS_PER_VIAL) 
+// #define NUM_CLIENTS 10 //Smaller values to shorten testing
 #define NUM_NURSES 10
 #define NUM_STATIONS NUM_NURSES
 #define NUM_REGISTRATIONS_SIMULTANEOUSLY 4
 
 
 /* global variables */
-pthread_mutex_t client_buffer_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t nurse_mutex = PTHREAD_MUTEX_INITIALIZER;
+//This mutex is used to protect the usable_vials variable 
+//when multiple nurses are done with their current vial at the same time
+pthread_mutex_t vial_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+//nurse and client registration semaphores are used to sync 4 clients with 4 nurses
 sem_t nurse_reg;
 sem_t client_reg;
+
+//station semaphore is used to ensure only 4 nurses are at the 4 station
+sem_t station_s;
+
 int status;
-long int client_array[5];
-int count = 0; 
-int in = 0;
-int out = 0;
+
+//usable_vials is used to ensure that the program is not edditing a constant
 int usable_vials = NUM_VIALS;
 
 int get_rand_in_range(int lower, int upper) {
@@ -59,90 +68,89 @@ void walk(int lower, int upper) {
 
 // arg is the nurses station number.
 void *nurse(void *arg) {
+    int current_vial;
     long int id = (long int)arg;
-
-    fprintf(stderr, "%s: nurse %ld started\n", curr_time_s(), id);
-    while(usable_vials != 0){
-        walk(1, 3);
-        pthread_mutex_lock(&nurse_mutex);
-        usable_vials--;
-        pthread_mutex_unlock(&nurse_mutex);
-        //for(int i = 0; i < 6; i++){
-            while( 0 != pthread_mutex_lock(&client_buffer_mutex)){
-
+                fprintf(stderr, "%s: nurse %ld started\n", curr_time_s(), id);
+    //while(true) did not work so this was a hack to get around that
+    while (1==1){
+            //a spin lock for if the mutex is held by another thread
+            while (0 != pthread_mutex_lock(&vial_mutex)){
+            
             }
-            if (out != in){
-                
-                    int working_client = client_array[out];
-                    fprintf(stderr, "%s: nurse %ld, is vaccinating client %i\n", curr_time_s(), id, working_client);
-                    out = (out + 1) % 5;
-                    count--;
-                pthread_mutex_unlock(&client_buffer_mutex);
-
+            walk(1, 3);
+            //if statement checks if there are any vials left, and if there are none left, the nurse thread leaves
+            if (usable_vials > 0){
+                 current_vial = usable_vials; 
+                usable_vials --;
+            }else{
+                pthread_mutex_unlock(&vial_mutex);
+                fprintf(stderr, "%s: nurse %ld is done\n", curr_time_s(), id);
+                pthread_exit(NULL);
+                return 0;
             }
-        //}
+            fprintf(stderr, "%s: nurse %ld is getting vial %i\n", curr_time_s(), id, current_vial);
+            walk(1, 3);
+            pthread_mutex_unlock(&vial_mutex);
+
+            //makes sure that only four nurses are at stations at a time
+            sem_wait(&station_s);
+            for(int shots_left = SHOTS_PER_VIAL; shots_left > 0; shots_left--){
+                //the rendezvous with client threads
+                sem_post(&client_reg);
+                fprintf(stderr, "%s: nurse %ld is waiting for client\n", curr_time_s(), id);
+                sem_wait(&nurse_reg);
+                fprintf(stderr, "%s: there are %i shots left in vial %i\n", curr_time_s(), shots_left - 1, current_vial);
+            }  
+            sem_post(&station_s);
     }
 
-    fprintf(stderr, "%s: nurse %ld is done\n", curr_time_s(), id);
-    pthread_exit(NULL);
 }
 
 void *client(void *arg) {
     long int id = (long int)arg;
-    sem_wait(&client_reg);
+
     fprintf(stderr, "%s: client %ld has arrived and is walking to register\n",
             curr_time_s(), id);
     walk(3, 10);
-    fprintf(stderr, "%s: client %ld is regestering\n", curr_time_s(), id);
+
+    fprintf(stderr, "%s: client %ld is regsitering\n", curr_time_s(), id);
     walk(3, 10);
-    
-    while(0 != pthread_mutex_lock(&client_buffer_mutex)){
-        
-    }
-    if (out != ((in + 1) % 5) || out == in){
-#if DEBUG
-    fprintf(stderr, "in is: %i\ncount is: %i\n", in, count);
-#endif
-        client_array[in] = id;
-        in = (in + 1) % 5;
-        count++;
-        fprintf(stderr, "%s: client %ld has registered, and is waiting to get a vaccine\n", curr_time_s(), id);
-        pthread_mutex_unlock(&client_buffer_mutex);
-    }else{
-        
-#if DEBUG
-    fprintf(stderr, "BUFFER is full\nin is: %i\ncount should equal 4 = %i\n", in, count);
-#endif
-    }
-    
-     
-    
+
+    //the rendezvous with the nurse threads
+    sem_wait(&client_reg);
+    fprintf(stderr, "%s: client %ld is getting their vaccine\n", curr_time_s(), id);
+    sem_post(&nurse_reg);
 
     fprintf(stderr, "%s: client %ld leaves the clinic!\n", curr_time_s(), id);
-    sem_post(&client_reg);
+
 
     pthread_exit(NULL);
 }
 
 int main() {
-    
-    status = sem_init(&nurse_reg, 0, 4);
-    status = sem_init(&client_reg, 0, 4);
+    //Initializing the rendezvous semaphores 
+    //acts as kind of a pseudo bounded buffer
+    status = sem_init(&nurse_reg, 0, 0);
+    status = sem_init(&client_reg, 0, 0);
+    //Initializes the station semaphore so only four nurses can access the stations at a time
+    status = sem_init(&station_s, 0, 4);
     srand(time(0));
+    //Initializing the nurse and client threads with sizes defined at start of file
     pthread_t nurse_threads[NUM_NURSES];
     pthread_t client_threads[NUM_CLIENTS];
 
-    pthread_create(&client_threads[0], NULL, client, NULL);
-    pthread_create(&nurse_threads[0], NULL, nurse, NULL);
-    // for(int i = 0; i < NUM_CLIENTS; i++){
-    //     walk(0, 1);
-    //     void *number = (void *)(intptr_t)i;
-    //     pthread_create(&client_threads[i], NULL, client, number);
-    // }
-    // for(int i = 0; i < NUM_NURSES; i++){
-    //     void *number = (void *)(intptr_t)i;
-    //     pthread_create(&nurse_threads[i], NULL, nurse, number);
-    // }
+    //creates the correct number of nurse threads
+    for(int i = 0; i < NUM_NURSES; i++){
+        void *number = (void *)(intptr_t)i;
+        pthread_create(&nurse_threads[i], NULL, nurse, number);
+    }
+    //creates the correct number of client threads with a randomly generated delay
+    for(int i = 0; i < NUM_CLIENTS; i++){
+        walk(0, 1);
+        void *number = (void *)(intptr_t)i;
+        pthread_create(&client_threads[i], NULL, client, number);
+    }
+    
     
 
     pthread_exit(0);
