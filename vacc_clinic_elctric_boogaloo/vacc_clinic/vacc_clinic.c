a8feb5df85d14131265dc05129c3537643ee63e8
zer0relm <zer0relm@gmail.com>
2021-04-15 08:24:59 -0400

    Working need to be cleaned

--- vacc_clinic_elctric_boogaloo/vacc_clinic/vacc_clinic.c
+++ vacc_clinic_elctric_boogaloo/vacc_clinic/vacc_clinic.c
@@ -10,16 +10,16 @@
 #include <time.h>
 #include <unistd.h>
 
-#define DEBUG 1
+#define DEBUG 0
 
-//#define NUM_VIALS 30
-#define NUM_VIALS 1
+#define NUM_VIALS 30
+//#define NUM_VIALS 1
+#define SHOTS_PER_VIAL 6
 //#define SHOTS_PER_VIAL 6
-#define SHOTS_PER_VIAL 2
-//#define NUM_CLIENTS (NUM_VIALS * SHOTS_PER_VIAL)
-#define NUM_CLIENTS 5
-//#define NUM_NURSES 10
-#define NUM_NURSES 1
+#define NUM_CLIENTS (NUM_VIALS * SHOTS_PER_VIAL)
+//#define NUM_CLIENTS 6
+#define NUM_NURSES 10
+//#define NUM_NURSES 1
 #define NUM_STATIONS NUM_NURSES
 #define NUM_REGISTRATIONS_SIMULTANEOUSLY 4
 
@@ -77,24 +77,52 @@ void *nurse(void *arg) {
     fprintf(stderr, "%s: nurse %ld started\n", curr_time_s(), id);
 
 
-    int used = 0;
-    walk(1, 3);
-    pthread_mutex_lock(&vial_mutex);
-    if (number_vials != 0 ){
-        fprintf(stderr, "%s: nurse %ld is getting vial %i\n", curr_time_s(), id, number_vials);
-        number_vials--;
-        pthread_mutex_unlock(&vial_mutex);
-    }else{
-        pthread_mutex_unlock(&vial_mutex);
-        fprintf(stderr, "%s: nurse %ld is leaving\n", curr_time_s(), id);
-        pthread_exit(NULL);
-    }
+    while(1){
+        walk(1, 3);
+        pthread_mutex_lock(&vial_mutex);
+        if (number_vials != 0 ){
+            fprintf(stderr, "%s: nurse %ld is getting vial %i\n", curr_time_s(), id, number_vials);
+            number_vials--;
+            pthread_mutex_unlock(&vial_mutex);
+        }else{
+            pthread_mutex_unlock(&vial_mutex);
+            fprintf(stderr, "%s: nurse %ld is leaving\n", curr_time_s(), id);
+            pthread_exit(NULL);
+        }
+        
+        for(unsigned i = 0; i < shots; i++){
+            sem_wait(&rvs2);
+        
+    #if DEBUG
+        fprintf(stderr, "nurse %ld is at rendezvous\n", id);
+    #endif
 
-    sem_post(&rvs1);
-    sem_wait(&rvs2);
+            
+            //move this
+            pthread_mutex_lock(&registration_mutex);
 
+    #if DEBUG
+        fprintf(stderr, "nurse %ld entered the mutex\nrb_out = %i, rb_count = %i\n", id, rb_out, rb_count);
+    #endif
 
-    
+            long int current_client = registration_buffer[rb_out];
+            rb_out = (rb_out + 1) %rb_size;
+            rb_count--;
+
+    #if DEBUG
+        fprintf(stderr, "rb_out = %i, rb_count = %i\n", rb_out, rb_count);
+    #endif
+
+            pthread_mutex_unlock(&registration_mutex);
+            fprintf(stderr, "%s: nurse %ld is vaccinating client %ld\n", curr_time_s(), id, current_client);
+
+            sem_post(&rvs1);
+        
+    #if DEBUG
+        fprintf(stderr, "nurse %ld left the rendezvous\n", id);
+    #endif
+        } 
+    }   
 }
 
 void *client(void *arg) {
@@ -108,30 +136,45 @@ void *client(void *arg) {
 #if DEBUG
     fprintf(stderr, "%s: client %ld is in the semaphor\n", curr_time_s(), id);
 #endif
-    if(rb_count == rb_size){
-        fprintf(stderr, "%s: rb_count %i == rb_size %i");
-    }else {
+    while(rb_count == rb_size){
+        fprintf(stderr, "rb_count %i == rb_size %i", rb_count, rb_size);
         
-        fprintf(stderr, "%s: client %ld is registering at desk %i\n",
-                curr_time_s(), id, rb_in);
+    }
+        
+        
+        walk(3, 10);
+
 #if DEBUG
     fprintf(stderr, "%s: before: client_id: %i.  rb_in: %i.  rb_count:  %i.\n", curr_time_s(), id, rb_in, rb_count);
 #endif
+
         pthread_mutex_lock(&registration_mutex);
+        fprintf(stderr, "%s: client %ld is registering at desk %i\n",
+                curr_time_s(), id, rb_in);
         registration_buffer[rb_in] = id;
         rb_in = (rb_in + 1) %rb_size;
         rb_count++;
         pthread_mutex_unlock(&registration_mutex);
+        walk(3, 10);
+
 #if DEBUG
     fprintf(stderr, "%s: after: client_id: %i rb_in: %i.  rb_count:  %i.\n", curr_time_s(), id, rb_in, rb_count);
 #endif
-        
-    }
-    sem_post(&client_semaphore);
- 
-    sem_wait(&rvs2);
-    sem_post(&rvs1);
     
+    sem_post(&rvs2);
+    
+#if DEBUG
+    fprintf(stderr, "%s: client %ld entered the rendezvous\n", curr_time_s(), id);
+#endif
+
+    sem_wait(&rvs1);
+
+#if DEBUG
+    fprintf(stderr, "%s: client %ld left the rendezvous\n", curr_time_s(), id);
+#endif
+
+    
+    sem_post(&client_semaphore);
 
     fprintf(stderr, "%s: client %ld leaves the clinic!\n", curr_time_s(), id);
 
