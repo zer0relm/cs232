dbaa89ef0f17c162e39c824d0097d5ce02fd0199
zer0relm <zer0relm@gmail.com>
2021-04-29 19:02:25 -0400

    it works

--- ceaser_cypher/cypher_client.py
+++ ceaser_cypher/cypher_client.py
@@ -1,64 +1,64 @@
+'''
+Author: AJ Vrieland (ajv234)
+Date: 04/29/2021
+'''
 import socket
 
-class cypher_client:
-    def __init__(self, serverName, port):
-        self._my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        self._server_name = serverName
-        self._port = port
+
+class cypher_client:
+    def __init__(self):
+        self._my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(30)
         self._timeout = socket.getdefaulttimeout()
-        try: 
-            if(self._server_name == "localhost" or self._server_name == ""):
-                self._my_socket.connect((socket.gethostname(), self._port))
-                welcome = self._my_socket.recv(1024)
-                print(welcome.decode())
-            else:
-                self._my_socket.connect((self._server_name, self._port))
-                welcome = self._my_socket.recv(1024)
-                print(welcome.decode())
-        except:
-            print(f"server: {self._server_name} and port: {self._port} is not connecting")
+        self.debug = 1
+        while True:
+            self._server_name = input("Welcome please input Server name or q to quit: ")
+            if (self._server_name == "q"):
+                    quit(0)
+            try: 
+                if(self._server_name == "localhost" or self._server_name == ""):
+                    self._port = int(input("please input the Port: "))
+                    self._my_socket.connect((socket.gethostname(), self._port))
+                    welcome = self._my_socket.recv(1024)
+                    print(welcome.decode())
+                    self.run("local")
+                    self._my_socket.close()
+                else:
+                    self._port = int(input("please input the Port: "))
+                    self._my_socket.connect((self._server_name, self._port))
+                    self.run("brooks")
+                    self._my_socket.close()
+
+            except:
+                print(f"server: {self._server_name} and port: {self._port} is not connecting")
 
     '''
     Messages sent between Python and Java, the python string must include \r\n because Java is expecting it but python does not
     automatically add those to the end of a string like java
     '''
-    # def run(self):  #This was a hardcode test to make sure it could connect to the server
-    #     try:
-    #         print(bytes("3\r\n", "utf-8"))
-    #         self._my_socket.send(bytes("10\r\n", "utf-8"))
-    #         self._my_socket.send(bytes("Rotate\r\n", "utf-8"))
-    #         # self._my_socket.send(bytes("2\r\n"))
-    #         print("socket sent, now recieving")
-    #         message = self._my_socket.recv(1024)
-    #         print(message.decode())
-    #         rotated = self._my_socket.recv(1024)
-    #         print(rotated.decode())
-    #     except:
-    #         print(f"send did not work")
-    #     # self._mssg = self._my_socket.recv(2)
-    #     self._my_socket.close()
 
-    def run(self):
+    def run(self, server):
         choice = "c"
-        
-        
+        #Rotation Group
+        ######
+        rotation = input("Input rotation amount: ")
+        self._my_socket.send(bytes(rotation + "\n", "utf-8"))
+        EncodedMessage = self._my_socket.recv(1024)
+        if self.debug == 1:
+            print(f"Initial message from server: {EncodedMessage}")
+        #######
         while (choice != "q"):
             try:
-                #Rotation Group
-                ######
-                rotation = input("Input rotation amount: ")
-                self._my_socket.send(bytes(rotation + "\r\n", "utf-8"))
-                EncodedMessage = self._my_socket.recv(1024)
-                #######
-
                 unEncodedMessage = input("Input message to encode: ")
                 
-                self._my_socket.send(bytes(unEncodedMessage + "\r\n", "utf-8"))
-                
+                self._my_socket.sendall(bytes(unEncodedMessage + "\n", "utf-8"))
                 
                 EncodedMessage = self._my_socket.recv(1024)
+                if server == "brooks":
+                    EncodedMessage = self._my_socket.recv(4096)
+                if self.debug == 1:
+                    print(f"encoded message directly is: {EncodedMessage}")
                 print(EncodedMessage.decode())
                 choice = input("Type q to quit\nType c to continue: ")
                 if(choice == "q"):
@@ -68,17 +68,9 @@ class cypher_client:
                 print(f"Error: Input must be between 1 and 25")
                 choice = input("Type q to quit\nType c to continue: ")
                 if(choice == "q"):
-                    quit
+                    continue
 
         
 
-
-# host = input("Hello, please input host: ")
-# port = int(input("please input port: "))
-host = "brooks.cs.calvin.edu"
-# host = "localhost"
-port = 9876
-
-cypher = cypher_client(host, port)
-cypher.run()
+cypher = cypher_client()
     
--- ceaser_cypher/cypher_server.py
+++ ceaser_cypher/cypher_server.py
@@ -1,4 +1,9 @@
+'''
+Author: AJ Vrieland (ajv234)
+Date: 04/29/2021
+'''
 import socket
+import time
 from string import ascii_letters as letters
 
 class server:
@@ -13,56 +18,49 @@ class server:
         self._alphabet = "abcdefghijklmnop"
 
     def run(self):
-        try:
+        while True:
             self._my_socket.listen(5)
-            self._client_socket, self._client_address = self._my_socket.accept()
-            self._client_socket.send(bytes("Welcome to the server!", "utf-8"))
-            print(f"connection from {self._client_address} has been established")
-
-
-            while True:
-
-                #Rotation group
+            try:
+                self._client_socket, self._client_address = self._my_socket.accept()
+                self._client_socket.send(bytes("Welcome to the server!", "utf-8"))
+                print(f"connection from {self._client_address} has been established")
+                #This section of code is blocked of because I can move this with it's equivilant in the client, into the below while loop
+                #and it allows the change of the rotation amount for each message without having to reconnect to the server
+                #however since that is not how brooks.cs.calvin.edu works, it is out side of the loop so that the client can connect to the other server
                 ##############
                 self._msg = self._client_socket.recv(1024)
-                print(f"1st sent is: {self._msg}")
-                number = self._msg.decode()
-                self._client_socket.send(bytes(number + "\r\n", "utf-8"))
-                number = number.split("\r\n")
-                self._msg2 = self._client_socket.recv(1024)
+                rotation_amount = self._msg.decode()
+                self._client_socket.send(bytes(rotation_amount + "\r\n", "utf-8"))
+                rotation_amount = rotation_amount.split("\r\n")
+                rotation = int(rotation_amount[0])
                 ##############
-
-                print(f"2nd sent is: {self._msg2}")
-
-                number2 = self._msg2.decode()
-
-                number2 = number2.split("\r\n")
-                number2 = number2[0].split()
-
-                print(f"Split string is: {number2} ")
-                print(f"rotation is {rotation}")
-                # print(f"Length of number2 is: {number2.__len__()}")
-                for i in number2:
-                    print(i)
-                    rotated_message += self.rotate(rotation, number2[i])
-                print(f"rotated message is {rotated_message}")
-
-
-                self._client_socket.send(bytes(rotated_message + "\r\n", "utf-8"))
-            self._my_socket.close()
-        except:
-            print(f"Exception thrown")
-            self._my_socket.close()
-            self._client_socket.close()
+                while True:
+                    self._msg2 = self._client_socket.recv(1024)
+                    message_to_encode = self._msg2.decode()
+                    message_to_encode = message_to_encode.split("\r\n")
+                    message_to_encode_list = message_to_encode[0].split()
+                    unRotatedMessage = ""
+                    rotated_message = ""
+                    for i in message_to_encode_list:
+                        unRotatedMessage += i + " "
+                        rotated_message += self.rotate(rotation, i)
+                        rotated_message += " "
+                    print(f"rotation is {rotation}")
+                    print(f"unrotated message is {unRotatedMessage}")
+                    print(f"rotated message is {rotated_message}\n\n")
+                    self._client_socket.send(bytes(rotated_message + "\r\n", "utf-8"))
+                self._my_socket.close()
+            except:
+                print(f"Exception thrown")
+                time.sleep(2)
+                continue
 
     def rotate(self, rotation, message):
         encrypted_message = ""
         rotate = int(rotation)
         for char in message:
-            print(letters.find(char), rotate)
             index = (letters.find(char) + rotate) % 26
             encrypted_message += letters[index]
-        print(encrypted_message)
         return encrypted_message
 
 
